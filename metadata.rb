2ac0c2ef8cf91e09fe5029a2101f384ee9d51f78
James Walker <walkah@walkah.net>
2013-11-29 14:27:24 -0500

    add chef-client cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,10 @@ maintainer_email 'walkah@walkah.net'
 license          'Apache 2.0'
 description      'Installs/Configures my various servers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 supports "ubuntu"
 
 depends "apt",          "~> 2.3.0"
+depends "chef-client",  "~> 3.1.0"
 depends "minecraft"
